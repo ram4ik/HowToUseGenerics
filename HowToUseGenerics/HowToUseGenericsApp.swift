c675ede09d580c2e171981db1295129ab6ac9f93
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-09-21 17:35:28 +0300

    Initial Commit

---
+++ HowToUseGenerics/HowToUseGenericsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToUseGenericsApp.swift
+//  HowToUseGenerics
+//
+//  Created by ramil on 21.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct HowToUseGenericsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
