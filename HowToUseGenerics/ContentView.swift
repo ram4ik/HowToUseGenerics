dc90794dedcf496ba4141916bedc80e6ff81adbc
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-09-21 18:17:35 +0300

    generics

--- HowToUseGenerics/ContentView.swift
+++ HowToUseGenerics/ContentView.swift
@@ -7,34 +7,36 @@
 
 import SwiftUI
 
+struct GenericModel<T> {
+    let info: T?
+    func removeInfo() -> GenericModel {
+        GenericModel(info: nil)
+    }
+}
+
 struct StringModel {
-    
     let info: String?
-    
     func removeInfo() -> StringModel {
         StringModel(info: nil)
     }
 }
 
-
 class GenericsViewModel: ObservableObject {
-    
     @Published var stringModel = StringModel(info: "Hello, world!")
+    @Published var genericStringModel = GenericModel(info: "Hello,  Generics!")
     
     func removeData() {
         stringModel = stringModel.removeInfo()
+        genericStringModel = genericStringModel.removeInfo()
     }
 }
 
-
-
 struct ContentView: View {
-    
     @StateObject private var vm = GenericsViewModel()
     
     var body: some View {
         VStack {
-            Text(vm.stringModel.info ?? "no data")
+            Text(vm.genericStringModel .info ?? "no data")
                 .onTapGesture {
                     vm.removeData()
                 }
